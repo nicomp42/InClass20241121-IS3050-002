65e5e5654885d722118d0a71135cf460d1a658c4
nicomp42 <nicholdw@ucmail.uc.edu>
2024-11-21 10:36:07 -0500

    Added a message to SayHello in HelloWorld class

--- InClass20241121-IS3050-002/HelloWorld.cs
+++ InClass20241121-IS3050-002/HelloWorld.cs
@@ -7,5 +7,10 @@ namespace InClass20241121_IS3050_002
 {
     public class HelloWorld
     {
+        public static void SayHello()
+        {
+            Console.WriteLine("Hello from HelloWorld class");
+            Console.WriteLine("I like pizza");
+        }
     }
 }
